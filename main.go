e8715e63a382b5bb1c142f9e99aba89b5625c077
Jacob Kepford <rtbredge@gmail.com>
2023-09-23 21:15:40 -0500

    Testing did win functionality

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package main
 
-import "sync"
+type Gamer interface {
+	PlayGame() bool
+}
 
 type Game struct {
-	mu   sync.Mutex
 	wins int
 }
 
 func (g *Game) AddWin() {
-	g.mu.Lock()
-	defer g.mu.Unlock()
 	g.wins++
 }
 
 func (g *Game) Wins() int {
 	return g.wins
+}
+
+func (g *Game) DetermineWin(gamer Gamer) bool {
+	return gamer.PlayGame()
 }
 
 func NewGame() *Game {
--- main.go_test.go
+++ main.go_test.go
@@ -1,77 +1,115 @@
 package main
 
 import (
-	"sync"
+	"math/rand"
 	"testing"
 )
 
+type TestAlwaysWinsGamer struct{}
+
+func (t TestAlwaysWinsGamer) PlayGame() bool {
+	return true
+}
+
+type TestRandomGamer struct{}
+
+func (t TestRandomGamer) PlayGame() bool {
+	return rand.Intn(4) == 1
+}
+
 func TestIncrementWin(t *testing.T) {
-	t.Run("Test Incrementing Wins One At A Time", func(t *testing.T) {
+	game := NewGame()
+	gameCount := 3
+
+	for i := 0; i < gameCount; i++ {
+		game.AddWin()
+	}
+
+	wantedWinCount := gameCount
+
+	assertWinCount(t, game, wantedWinCount)
+}
+
+func TestDetermineWin(t *testing.T) {
+	t.Run("Test Synchronous Dynamic Win", func(t *testing.T) {
 		game := NewGame()
-		gameCount := 1000000
+		gamer := TestAlwaysWinsGamer{}
+		gameCount := 3
 
 		for i := 0; i < gameCount; i++ {
-			game.AddWin()
-		}
-
-		wantedWinCount := gameCount
-
-		assertWinCount(t, game, wantedWinCount)
-
-	})
-
-	t.Run("Test Incrementing Wins Concurrently", func(t *testing.T) {
-		game := NewGame()
-		gameCount := 1000000
-		var wg sync.WaitGroup
-
-		wg.Add(gameCount)
-
-		for i := 0; i < gameCount; i++ {
-			go func() {
+			didWin := game.DetermineWin(gamer)
+			if didWin {
 				game.AddWin()
-				wg.Done()
-			}()
+			}
 		}
-
-		wg.Wait()
 
 		assertWinCount(t, game, gameCount)
 	})
 
+	// t.Run("Test Concurrent Dynamic Win", func(t *testing.T) {
+	// 	game := NewGame()
+	// 	gamer := TestAlwaysTrueGamer{}
+	// 	gameCount := 1000000
+	// 	var wg sync.WaitGroup
+
+	// 	wg.Add(gameCount)
+
+	// 	for i := 0; i < gameCount; i++ {
+	// 		go func() {
+	// 			didWin := game.DetermineWin(gamer)
+	// 			if didWin {
+	// 				game.AddWin()
+	// 			}
+	// 			wg.Done()
+	// 		}()
+	// 	}
+
+	// 	wg.Wait()
+
+	// 	assertWinCount(t, game, gameCount)
+	// })
 }
 
-func BenchmarkWins(b *testing.B) {
-	b.Run("Test Wins One At A Time", func(b *testing.B) {
-		game := NewGame()
-		gameCount := 1000000
+// func BenchmarkDynamicWinsIfConcurrentIsFaster(b *testing.B) {
+// 	b.Run("Wins One At A Time", func(b *testing.B) {
+// 		game := NewGame()
+// 		gamer := TestRandomGamer{}
+// 		gameCount := 100000
 
-		b.ResetTimer()
+// 		b.ResetTimer()
 
-		for i := 0; i < gameCount; i++ {
-			game.AddWin()
-		}
-	})
+// 		for i := 0; i < gameCount; i++ {
+// 			didWin := game.DetermineWin(gamer)
+// 			if didWin {
+// 				game.AddWin()
+// 			}
+// 		}
+// 	})
 
-	b.Run("Test Wins Concurrently", func(b *testing.B) {
-		game := NewGame()
-		gameCount := 1000000
-		var wg sync.WaitGroup
+// 	b.Run("Concurrent Dynamic Wins", func(b *testing.B) {
+// 		game := NewGame()
+// 		gamer := TestRandomGamer{}
+// 		gameCount := 100000
+// 		var wg sync.WaitGroup
 
-		wg.Add(gameCount)
+// 		wg.Add(gameCount)
 
-		b.ResetTimer()
+// 		b.ResetTimer()
 
-		for i := 0; i < gameCount; i++ {
-			go func() {
-				game.AddWin()
-				wg.Done()
-			}()
-		}
+// 		for i := 0; i < gameCount; i++ {
+// 			go func() {
+// 				didWin := game.DetermineWin(gamer)
+// 				if didWin {
+// 					game.AddWin()
+// 				}
+// 				wg.Done()
+// 			}()
+// 		}
 
-		wg.Wait()
-	})
-}
+// 		wg.Wait()
+
+// 	})
+// }
 
 func assertWinCount(t testing.TB, game *Game, wantedWinCount int) {
 	t.Helper()
